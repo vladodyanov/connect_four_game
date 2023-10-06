bb1d193d7e7ba25fb0b130a71a9761885b5a567b
Vladimir Dyanov <133800606+vladodyanov@users.noreply.github.com>
2023-10-06 20:38:14 +0300

    Create console_connect_four.py
    revison 1

---
+++ console_connect_four.py
@@ -0,0 +1,110 @@
+ROWS = 5
+COLS = 7
+
+direction_mapper = [
+    (-1, 0), #up
+    (1, 0), #down
+    (0, 1), #right
+    (0, -1), #left
+    (-1, 1), #up right
+    (1, -1), #down left
+    (-1, -1), #up left
+    (1, 1,) #down right
+]
+
+class FullColumnError(Exception):
+    pass
+
+
+def print_matrix(matrix):
+    for row in matrix:
+        print(row)
+
+
+def is_valid_column_choice(selected_column_index):
+    if 0 <= selected_column_index < COLS:
+        return True
+    return False
+
+
+def place_player_number(column_index, matrix, player_number):
+    for row_index in range(ROWS-1, -1 ,-1):
+        if matrix[row_index][column_index] == 0:
+            matrix[row_index][column_index] = player_number
+            return row_index, column_index
+            break
+    else:
+        raise  FullColumnError
+
+
+def requested_direction_count(current_row, current_col, row_movement, col_movement, matrix, player):
+    count = 1
+    for i in range(1, 4):
+        row_index_to_check = current_row + row_movement * i
+        col_index_to_check = current_col + col_movement * i
+        if not is_valid_place(row_index_to_check, col_index_to_check):
+            break
+        if (matrix[row_index_to_check][col_index_to_check] == 0 or
+                matrix[row_index_to_check][col_index_to_check] != player):
+            break
+        count += 1
+    return count
+
+
+def oposite_direction_count(current_row, current_col, row_movement, col_movement, matrix, player):
+    count = 0
+    for i in range(1, 4):
+        row_index_to_check = current_row - row_movement * i
+        col_index_to_check = current_col - col_movement * i
+        if not is_valid_place(row_index_to_check, col_index_to_check):
+            break
+        if (matrix[row_index_to_check][col_index_to_check] == 0 or
+                matrix[row_index_to_check][col_index_to_check] != player):
+            break
+        count += 1
+    return count
+
+
+def is_winner(current_row, current_col, matrix, player):
+    for row_movement, col_movement in direction_mapper:
+        count_direction = requested_direction_count(current_row, current_col, row_movement, col_movement, matrix, player)
+        count_oposite_direction = oposite_direction_count(current_row, current_col, row_movement, col_movement, matrix, player)
+        if (count_direction + count_oposite_direction) >= 4:
+            return True
+    return False
+
+
+def is_valid_place(row, col):
+    if 0 <= row < ROWS and 0 <= col < COLS:
+        return True
+    return False
+
+
+matrix = [[0 for _ in range (COLS)] for _ in range(ROWS)]
+print('You are playing "CONNECT 4" game, developed by Vladimir Dyanov')
+#GAME ON
+print_matrix(matrix)
+
+player = 1
+while True:
+    try:
+        selected_column_number = int(input(f"Player {player}, please choose a column: "))
+        selected_column_index = selected_column_number - 1
+        if not is_valid_column_choice(selected_column_index):
+            raise ValueError
+        current_row, current_col = place_player_number(selected_column_index, matrix, player)
+        if is_winner(current_row, current_col, matrix, player):
+            print(f"Player {player} just CONNECT 4 !")
+            print(f"Player {player} WINS !")
+            print_matrix(matrix)
+            break
+        print_matrix(matrix)
+    except ValueError:
+        print(f"Player {player}, please select number between 1 and {COLS} !")
+        continue
+    except FullColumnError:
+        print(f"Player {player}, this column is full, please select another one")
+        continue
+
+    player += 1
+    player = 2 if player % 2 == 0 else 1
